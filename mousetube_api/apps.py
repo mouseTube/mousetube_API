ebc01c03c490e98b946f59330d6c037ef0b017ee
lbouri <laurent.bouri2@gmail.com>
2025-06-25 09:10:51 +0000

    fix: activate signals

--- mousetube_api/apps.py
+++ mousetube_api/apps.py
@@ -5,6 +5,4 @@ class MousetubeApiConfig(AppConfig):
     name = "mousetube_api"
 
     def ready(self):
-        pass
-        # Ensure that signals are imported when the app is ready
-        # This will register the signal handlers defined in utils/signals.py
+        import mousetube_api.utils.signals
---
+++ mousetube_api/utils/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'mousetube_api.apps.MousetubeApiConfig'
