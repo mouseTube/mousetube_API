cba6fddb07fad5b3e1f2cba32bf349d42c68d24f
lbouri1 <laurent.bouri2@gmail.com>
2025-10-17 13:37:06 +0200

    fix: ruff format

--- mousetube_api/__init__.py
+++ mousetube_api/__init__.py
@@ -5,6 +5,7 @@ from .celery import app as celery_app
 
 __all__ = ["celery_app"]
 
+
 def manage():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mousetube_api.settings")
     from django.core.management import execute_from_command_line
